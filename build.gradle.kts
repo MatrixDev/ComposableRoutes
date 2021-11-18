0c175eb0d4b0d85478ab850ec6c55cc0d685c391
matrixdev <rostyslav.lesovyi@enghouse.com>
2021-11-18 20:47:49 +0200

    changed version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     val kotlinVersion by extra { "1.5.31" }
-    val composeRoutesVersion by extra { "0.1.10" }
+    val composeRoutesVersion by extra { "0.1.11" }
 
     repositories {
         google()
